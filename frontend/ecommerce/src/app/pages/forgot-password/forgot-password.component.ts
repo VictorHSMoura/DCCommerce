fea1f44dc104fe153a899fe1c4b252501dcb3b47
Lucas <lucaspaulom@hotmail.com>
2020-03-14 23:26:05 -0300

    fix: 🐛 Lint

--- frontend/ecommerce/src/app/pages/forgot-password/forgot-password.component.ts
+++ frontend/ecommerce/src/app/pages/forgot-password/forgot-password.component.ts
@@ -8,7 +8,7 @@ import { AppState } from 'src/app/stores/reducers';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent implements OnInit {
 
