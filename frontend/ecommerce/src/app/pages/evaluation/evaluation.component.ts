5ba72d2f0efdf9c2676b3ec99eb7c84f6abc2bd5
Julia Fonseca <42375312+jufs128@users.noreply.github.com>
2020-09-22 12:11:38 -0300

    Evalution personalized for product

--- frontend/ecommerce/src/app/pages/evaluation/evaluation.component.ts
+++ frontend/ecommerce/src/app/pages/evaluation/evaluation.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnChanges, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToasterService } from 'angular2-toaster';
-import { Product } from 'src/app/models/product.model';
+import { ActivatedRoute, Params, Router, RouterOutlet } from '@angular/router';
+import { MainProduct } from 'src/app/models/product.model';
+import { ProductService } from 'src/app/services/product/product.service';
 import { CATEGORIES } from '../../models/consts/categories';
 
 @Component({
   selector: 'app-evaluation',
   templateUrl: './evaluation.component.html',
-  styleUrls: ['./evaluation.component.scss']
+  styleUrls: ['./evaluation.component.scss'],
+  providers: [ProductService]
 })
 export class EvaluationComponent implements OnInit {
 
   public evaluationForm: FormGroup;
-  public money: number;
-  public categories: string[] = CATEGORIES;
+  public product: MainProduct;
+  public mainImage: string;
 
   constructor(
     private formBuilder: FormBuilder,
-    private toasterService: ToasterService
+    private toasterService: ToasterService,
+    private productService: ProductService,
+    private route: ActivatedRoute
   ) { }
 
   public async publicar(): Promise<void> {
@@ -39,6 +44,13 @@ export class EvaluationComponent implements OnInit {
 
   ngOnInit() {
     this.initForm();
+    this.route.params.subscribe((params: Params) => {
+      this.productService.getProductById(params.id)
+        .then((product: MainProduct) => {
+          this.product = product;
+          this.mainImage = product.imagens[0].url;
+        });
+    });
   }
 
 }
