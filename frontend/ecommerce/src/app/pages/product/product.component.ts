e456407a176a20ca19d5f45d752927353bc82c6c
Julia Fonseca <42375312+jufs128@users.noreply.github.com>
2020-09-23 01:19:01 -0300

    Adding Evaluation

--- frontend/ecommerce/src/app/pages/product/product.component.ts
+++ frontend/ecommerce/src/app/pages/product/product.component.ts
@@ -37,7 +37,7 @@ export class ProductComponent implements OnInit {
         comments.forEach(a => count += 1);
         comments.forEach(a => this.avaliacao += Number(a.nota));
         this.avaliacao = Math.round((this.avaliacao / count)*10)/10;
-        document.getElementById("nota").innerHTML = this.avaliacao;
+        document.getElementById("nota").innerHTML = this.avaliacao.toString();
       });
 
 
